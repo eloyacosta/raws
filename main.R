191e7e88e49609d9470d69e2f660c9832f463194
Eloy <eloy.acosta@gmail.com>
2023-06-01 20:34:11 +0000

    final

--- main.R
+++ main.R
@@ -1,14 +1,16 @@
 install.packages('aws.s3')
 library("aws.s3")
 
+# NOTE: read system environment again if session token expires 
 readRenviron("~/.Renviron")
 
 bucketlist()
 
+# get objects from buckets on the research gateway AWS account
 get_object("s3://rg-mgbrctdemo-ez957s3bucket-b5d/framingham.csv")
 s3load("framingham.csv", bucket = "rg-mgbrctdemo-ez957s3bucket-b5d")
 
-# get object to test access
+# get objects from buckets on the MGB AWS acccount 
 get_object("framingham.csv", bucket = "ez957-test0")
 # save file locally
 save_object("framingham.csv", file = "/home/ez957/framingham.csv", bucket = "ez957-test0")
