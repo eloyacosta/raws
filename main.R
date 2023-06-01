8b930defea737066da75e527a8106ca7cda94de3
eloyacosta <eloy.acosta@gmail.com>
2023-06-01 16:44:02 -0400

    Update main.R
    refactor

--- main.R
+++ main.R
@@ -6,10 +6,6 @@ readRenviron("~/.Renviron")
 
 bucketlist()
 
-# get objects from buckets on the research gateway AWS account
-get_object("s3://rg-mgbrctdemo-ez957s3bucket-b5d/framingham.csv")
-s3load("framingham.csv", bucket = "rg-mgbrctdemo-ez957s3bucket-b5d")
-
 # get objects from buckets on the MGB AWS acccount 
 get_object("framingham.csv", bucket = "ez957-test0")
 # save file locally
@@ -17,5 +13,10 @@ save_object("framingham.csv", file = "/home/ez957/framingham.csv", bucket = "ez9
 # import dataset 
 data <- read.csv("/home/ez957/framingham.csv")
 class(data)
-retval <- subset(data, age == max(age))   #This willextract the details of the oldest patient
+retval <- subset(data, age == max(age))   #This will extract the details of the oldest patient
 View(retval)
+
+# get objects from buckets on the research gateway AWS account
+# NOTE: NOT WORKING 
+get_object("s3://rg-mgbrctdemo-ez957s3bucket-b5d/framingham.csv")
+s3load("framingham.csv", bucket = "rg-mgbrctdemo-ez957s3bucket-b5d")
